6d8f01c633c383a1f8f5348eb6ff43449c517757
SCHILTZ Lucas <lucasschiltz.etude57@gmail.com>
2026-05-13 11:01:50 +0200

    feat: Add generic type T in parseCSv and readCSV method + add mapper params

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 import path from 'path';
 import { parseCsv } from './utils/csv-utils';
-const base = __dirname;
-const custPath = path.join(base, '../legacy/data', 'customers.csv');
-parseCsv(custPath);
 
--- src/utils/csv-utils.ts
+++ src/utils/csv-utils.ts
@@ -1,9 +1,9 @@
 import fs from 'fs';
-export const parseCsv = (content: string): string[][] => {
-    return content.split('\n').filter(l => l.trim()).slice(1).map(line => line.split(','));
+export const parseCsv = <T>(content: string, mapper: (parts: string[]) => T): T[] => {
+    return content.split('\n').filter(l => l.trim()).slice(1).map(line => mapper(line.split(',')));
 }
 
-export const readCsv = (filePath: string): string[][] => {
+export const readCsv = <T>(filePath: string, mapper: (parts: string[]) => T): T[] => {
     const content = fs.readFileSync(filePath, 'utf-8');
-    return parseCsv(content);
+    return parseCsv(content, mapper);
 }
