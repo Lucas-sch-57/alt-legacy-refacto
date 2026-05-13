a5ee6bf2e77b07e443bf6d3e67cf369549c926a0
SCHILTZ Lucas <lucasschiltz.etude57@gmail.com>
2026-05-13 11:17:04 +0200

    feat: Create the loadProducts function

--- src/services/data-loader.ts
+++ src/services/data-loader.ts
@@ -1,6 +1,7 @@
 import { Customer } from "../types/customer";
 import { readCsv } from "../utils/csv-utils";
 import {DATA_PATHS} from '../constants/constants';
+import { Product } from "../types/product";
 export const loadCustomers= (): Customer[] =>
     readCsv(DATA_PATHS.CUSTOMERS, (parts) => ({
         id: parts[0],
@@ -10,3 +11,12 @@ export const loadCustomers= (): Customer[] =>
         currency: parts[4] || 'EUR',
     }))
 
+export const loadProducts =(): Product[] =>
+    readCsv(DATA_PATHS.PRODUCTS, (parts)=>({
+           id: parts[0],
+    name: parts[1],
+    category: parts[2],
+    price: parseFloat(parts[3]),
+    weight: parseFloat(parts[4] || '1.0'),
+    taxable: parts[5] === 'true',
+    }))
