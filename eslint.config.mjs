819cc8f84f88759b0607d67e822eb4a1a1bac8c5
SCHILTZ Lucas <lucasschiltz.etude57@gmail.com>
2026-05-13 11:18:30 +0200

    dep: install eslint and add eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      'no-console': 'warn',
+      'no-magic-numbers': ['warn', { ignore: [0, 1, -1], ignoreArrayIndexes: true }],
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', 'legacy/'],
+  },
+];
