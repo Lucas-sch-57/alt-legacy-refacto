a531cce5d35fea178db88264d583b7f1225f81ad
SCHILTZ Lucas <lucasschiltz.etude57@gmail.com>
2026-05-13 11:09:33 +0200

    feat: write loadCustomers function

--- src/services/data-loader.ts
+++ src/services/data-loader.ts
@@ -1,6 +1,12 @@
 import { Customer } from "../types/customer";
 import { readCsv } from "../utils/csv-utils";
 import {DATA_PATHS} from '../constants/constants';
-export const loadCustomers= (): Customer[] {
-    readCsv(DATA_PATHS.CUSTOMERS)
-}
+export const loadCustomers= (): Customer[] =>
+    readCsv(DATA_PATHS.CUSTOMERS, (parts) => ({
+        id: parts[0],
+        name: parts[1],
+        level: (parts[2] || 'BASIC') as 'BASIC' | 'PREMIUM',
+        shipping_zone: parts[3] || 'ZONE1',
+        currency: parts[4] || 'EUR',
+    }))
+
