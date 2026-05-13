b3b1afcb1aa0b5af8d93d31390c6fd0d83441e33
SCHILTZ Lucas <lucasschiltz.etude57@gmail.com>
2026-05-13 10:29:21 +0200

    feat: Create a PATH constants that stock all files paths

---
+++ src/constants/path.ts
@@ -0,0 +1,7 @@
+export const PATH = {
+    CUSTOMERS: '../legacy/data/customers.csv',
+    PRODUCTS: '../legacy/data/products.csv',
+    ORDERS: '../legacy/data/orders.csv',
+    PROMOTIONS: '../legacy/data/promotions.csv',
+    SHIPPING_ZONES: '../legacy/data/shipping_zones.csv'
+}
